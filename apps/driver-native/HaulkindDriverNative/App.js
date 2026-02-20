31060aef2a9c76141ad7f96b1471fd807ec07379
Haulkind <support@haulkind.com>
2026-02-20 06:21:03 +0000

    Add OnboardingScreen to navigation stack

--- apps/driver-native/HaulkindDriverNative/App.js
+++ apps/driver-native/HaulkindDriverNative/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { LoginScreen, SignupScreen, HomeScreen } from "./src/screens";
+import { OnboardingScreen } from "./src/screens/OnboardingScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: true }}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
